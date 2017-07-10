fc99d54595503d0727a117b6a783aa81b151d220
Douglas Patriarche <doug@aianalysis.com>
2017-07-10 10:31:19 -0400

    Update niftyreg to 1.5.41

--- nifty-reg.rb
+++ nifty-reg.rb
@@ -1,17 +1,23 @@
 require "formula"
 
 class NiftyReg < Formula
+  # NiftyReg installation instructions: http://cmictig.cs.ucl.ac.uk/wiki/index.php/NiftyReg_install
   desc "Nifty-reg image registration program"
   homepage "http://niftyreg.sourceforge.net"
-  url "http://downloads.sourceforge.net/project/niftyreg/nifty_reg-1.3.9/nifty_reg-1.3.9.tar.gz"
-  version "1.3.9"
-  sha256 "34dfeae2afe86d736e196da573484acc5bf5ad804f8675b9387cd34074da691f"
+  head "https://sourceforge.net/code-snapshots/git/n/ni/niftyreg/git.git"
+  url "https://sourceforge.net/code-snapshots/git/n/ni/niftyreg/git.git/niftyreg-git-88b369becb6bdbda22ea96a0bef2b83bee85b219.zip"
+  version "1.5.41"
+  sha256 "8906a1f5082501cc1f219ddf6149ef17fe45563a51523dc314a016a8fc7de83e"
 
+  depends_on "doxygen"
   depends_on "libpng"
   depends_on "cmake" => :build
+  depends_on "gcc5" => :build
 
   def install
-    system "cmake", "-D", "CMAKE_BUILD_TYPE=Release", "."
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=" + prefix, "-DCMAKE_C_COMPILER=gcc-5", "-DCMAKE_CXX_COMPILER=g++-5", ".."
+      system "make", "install"
+    end
   end
 end
